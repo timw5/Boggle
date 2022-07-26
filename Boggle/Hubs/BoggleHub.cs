037583f00d374dd5147b677d240cbfebea051e89
timw5 <70343494+timw5@users.noreply.github.com>
2022-07-25 21:44:58 -0600

    added html for joining a game

--- Boggle/Hubs/BoggleHub.cs
+++ Boggle/Hubs/BoggleHub.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.Azure.SignalR;
 using Microsoft.AspNetCore.SignalR;
+using Boggle.Models;
+using AutoMapper;
 
 namespace Boggle.Hubs
 {
     public class BoggleHub: Hub
     {
+        private static List<Game> Games = new List<Game>();
+        private static List<User> Users = new List<User>();
+        private readonly IMapper Mapper;
+
+        public BoggleHub(IMapper mapper)
+        {
+            Mapper = mapper;
+        }   
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+        
+        public void AddUser(string name)
+        {
+            var user = new User(Context.ConnectionId, name);
+            Users.Add(user);
+        }
+
         
     }
 }
